e46403b129d8674ba2b218583efc1e790910657a
Luca SK <sarifluca@gmail.com>
2024-01-24 15:33:18 +0000

    blockchain with metamask implemented

--- src/routes/api/blockchainCommit/+server.ts
+++ src/routes/api/blockchainCommit/+server.ts
@@ -2,28 +2,76 @@ import { json } from '@sveltejs/kit';
 import fs from 'fs';
 import Web3 from 'web3';
 
-const blockchainPath = "/Users/god/Documents/thirdYear/cs310/humanmade/blockchain"
+/* const blockchainPath = "/Users/god/Documents/thirdYear/cs310/humanmade/blockchain"
 const abi = await fs.promises.readFile(`${blockchainPath}/ABI.json`, 'utf8');
 const contractAddr = await fs.promises.readFile(`${blockchainPath}/contractAddress.txt`, 'utf8');
-const ethNode = "HTTP://127.0.0.1:7545";
-//await fs.promises.readFile(`${blockchainPath}/ethNode.txt`, 'utf8');
+const ethNode = await fs.promises.readFile(`${blockchainPath}/ethNode.txt`, 'utf8');//"HTTP://127.0.0.1:7545";
 
-const web3 = new Web3(ethNode);
+/* const web3 = new Web3(ethNode);
+const contract = new web3.eth.Contract(JSON.parse(abi), contractAddr); */
 
-const contract = new web3.eth.Contract(JSON.parse(abi), contractAddr);
-
-// Function to make a commit
+/* // Function to make a commit
 async function makeCommit(commitHash: string, fromAddress: string) {
 	const receipt = await contract.methods.makeCommit(commitHash).send({ from: fromAddress });
 	return receipt.transactionHash;
 }
+ 
+// Check if MetaMask is available
+async function commit(){
+    if (typeof window.ethereum !== 'undefined') {
+        const web3 = new Web3(window.ethereum);
+        const contract = new web3.eth.Contract(JSON.parse(abi), contractAddr);
+        try {
+            // Request account access
+            await window.ethereum.enable();
+    
+            const accounts = await web3.eth.getAccounts();
+            const fromAddress = accounts[0]; // User's address
+    
+            // Create a transaction object
+            const txObject = {
+                to: contractAddr,
+                data: contract.methods.makeCommit('commitHash').encodeABI(),
+                gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
+                nonce: await web3.eth.getTransactionCount(fromAddress),
+                from: fromAddress,
+                gasLimit: '0'
+            };
+    
+            const gasEstimate = web3.utils.toHex(await web3.eth.estimateGas(txObject));
+            const gasBuffer = 1.2;
+            txObject.gasLimit = web3.utils.toHex(Number(gasEstimate) * gasBuffer);
+    
+            // MetaMask will handle signing and sending the transaction
+            const txHash = await web3.eth.sendTransaction(txObject);
+            console.log('Transaction Hash:', txHash);
+            return txHash;
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    } else {
+        console.log('MetaMask is not installed');
+    }    
+}
+
 
 export const POST = async ({ request }) => {
-	const data = await request.json();
-	const walletAddr = data;
+	
 
-	const resp = await makeCommit('69', walletAddr);
-	console.log('resp fom makeCommit: ' + resp);
+	return json(await commit());
+};
+ */
 
-	return json(resp);
+export const GET = async () => {
+	const blockchainPath = '/Users/god/Documents/thirdYear/cs310/humanmade/blockchain';
+	const abi = await fs.promises.readFile(`${blockchainPath}/ABI.json`, 'utf8');
+	const contractAddr = await fs.promises.readFile(`${blockchainPath}/contractAddress.txt`, 'utf8');
+	const ethNode = await fs.promises.readFile(`${blockchainPath}/ethNode.txt`, 'utf8'); //"HTTP://127.0.0.1:7545";
+
+    const info = {
+        abi: abi,
+        contractAddr: contractAddr,
+        ethNode: ethNode
+    }
+    return json(info);
 };
--- src/routes/creations/[slug]/+page.svelte
+++ src/routes/creations/[slug]/+page.svelte
@@ -32,6 +32,63 @@
 	import { onMount } from 'svelte';
 	import ShowcaseDiv from '$lib/components/ShowcaseDiv.svelte';
 
+	import fs from 'fs';
+	import Web3 from 'web3';
+
+	async function commit() {
+		console.log('commiting');
+		const deets = await fetch('../api/blockchainCommit', { method: 'GET' });
+		const { abi, contractAddr, ethNode } = await deets.json();
+
+		if (typeof window.ethereum !== 'undefined') {
+			console.log('metamask installed');
+			const web3 = new Web3(window.ethereum);
+			const contract = new web3.eth.Contract(JSON.parse(abi), contractAddr);
+			try {
+				// Request account access
+				await window.ethereum.enable();
+
+				const accounts = await web3.eth.getAccounts();
+				const fromAddress = accounts[0]; // User's address
+
+				// Create a transaction object
+				const txObject = {
+					to: contractAddr,
+					data: contract.methods.makeCommit('commitHash').encodeABI(),
+					//gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
+					nonce: await web3.eth.getTransactionCount(fromAddress),
+					from: fromAddress,
+					gasLimit: '0',
+					maxFeePerGas: '0',
+					maxPriorityFeePerGas: '0'
+				};
+				console.log('gas estimating:', txObject);
+
+				// Get current base fee and calculate appropriate maxFeePerGas
+				const latestBlock = await web3.eth.getBlock('latest');
+				const baseFee = latestBlock.baseFeePerGas;
+				const maxPriorityFeePerGas = web3.utils.toWei('2', 'gwei'); // Example priority fee
+				const maxFeePerGas = web3.utils.toHex(Number(baseFee) + Number(maxPriorityFeePerGas));
+
+				txObject.maxFeePerGas = maxFeePerGas;
+				txObject.maxPriorityFeePerGas = maxPriorityFeePerGas;
+
+				// Now estimate gas limit
+				const estimatedGas = await web3.eth.estimateGas(txObject);
+				const buffer = 1.2;
+				txObject.gasLimit = web3.utils.toHex(Math.ceil(buffer * Number(estimatedGas)));
+				// MetaMask will handle signing and sending the transaction
+				const txHash = await web3.eth.sendTransaction(txObject);
+				console.log('Transaction Hash:', txHash);
+				return txHash;
+			} catch (error) {
+				console.error('Error:', error);
+			}
+		} else {
+			console.log('MetaMask is not installed');
+		}
+	}
+
 	let firstCommit: Commit;
 
 	let selectedFiles: FileList;
@@ -91,10 +148,7 @@
 			percentage: creation.percentage
 		});
 
-		//log to blockchain lolems
-		const resp = await fetch('../api/blockchainCommit', { method: 'POST', body: JSON.stringify("0x84E62744aFA3687D1d9d997Bc515477454463731") });
-		const data = await resp.json();
-		console.log("dataa: ", data);
+		console.log('commit res: ', await commit());
 	};
 
 	let addEvidence = async () => {
@@ -108,8 +162,6 @@
 	}
 
 	onMount(() => {
-		console.log(creation.percentage, 'po');
-
 		const commitsInstance = onSnapshot(
 			query(collection(db, `creations/${creation.id}/commits`), orderBy('time', 'asc')),
 			(snapshot) => {
